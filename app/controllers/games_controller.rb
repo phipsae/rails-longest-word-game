a34d1e822dad40557c9e2df0ae5c12dced05bca4
Philip Krause <philip.krause88@yahoo.de>
2022-01-12 20:29:08 -0600

    game implemented

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+# require 'open-uri'
+# require 'json'
+
 class GamesController < ApplicationController
   def new
     def generate_grid(grid_size)
@@ -9,6 +12,41 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    @word = params[:word]
+    @grid = params[:tag_list]
+
+
+    def word_in_grid?(attempt, grid)
+      (attempt.to_s.split("") - grid.to_s.split("")).empty?
+    end
+
+    def english_word?(attempt)
+      url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+      user_serialized = URI.open(url).read
+      user = JSON.parse(user_serialized)
+      user['found']
+    end
+
+    def run_game(word, grid) # , start_time, end_time
+      # TODO: runs the game and return detailed hash of result (with `:score`, `:message` and `:time` keys)
+      # score = 0
+      # time = end_time - start_time
+      if word_in_grid?(word, grid) == false
+        message = "Sorry but #{word.upcase} cannot build out of #{@grid}"
+      elsif english_word?(word) == false
+        message = "Lost, #{word.upcase} isnt an English word"
+      else
+        # score = score_calc(time, attempt)
+        message = "Congratulations, you found #{word.upcase}"
+      end
+      { message: message } # score: score, , time: time
+    end
+
+    def score_calc(time, attempt)
+      attempt.length * (1.0 - (time / 60.0))
+    end
+
+    @result = run_game(@word, @grid)
+
   end
 end
